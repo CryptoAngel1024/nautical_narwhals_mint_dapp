fe32469a76400255adb55faf49bfea88f10af0eb
CryptoAngel1024 <cryptoangel1024@gmail.com>
2021-10-17 12:51:00 +0100

    hmtl_fix

--- frontend/src/Pages/Home/Mint.jsx
+++ frontend/src/Pages/Home/Mint.jsx
@@ -21,31 +21,31 @@ export default function Mint() {
         mintPublicSale
     ] = useWeb3Integration(counter);
 
-    return (
-        <Section className='mint' id='mint'>
-            <h3 className='section_title'><b>Mint</b>&nbsp; <span> {web3provider ? mintedAmount : '0'}/5757</span></h3>
-            <div className="mint_max">
-                <span>Max 10 per txn</span>
-                <div className="mint_input">
-                    <button disabled={counter <= 0 ? true : false} onClick={decrementCounter}>-</button>
-                    <input type="text" name="" id="" value={counter} maxLength={2} />
-                    <button disabled={counter >= 10 ? true : false} onClick={incrementCounter}>+</button>
-                </div>
-                <div className="mint_button">
-                    {
-                        isPaused ? (
-                            <button disabled >Mint is Paused</button>
-                        ) : (
-                            isPresale && !isPaused ? (
-                                <button onClick={() => mintPreSale(web3provider, counter)}>Mint Presale</button>
-                            ) : (
-                                <button onClick={() => mintPublicSale(web3provider, counter)}>Mint</button>
-                            )
-                        )
-                    }
-                    {/* <button onClick={mint}>Minit</button> */}
-                </div>
-            </div>
-        </Section>
+    // return (
+    //     <Section className='mint' id='mint'>
+    //         <h3 className='section_title'><b>Mint</b>&nbsp; <span> {web3provider ? mintedAmount : '0'}/5757</span></h3>
+    //         <div className="mint_max">
+    //             <span>Max 10 per txn</span>
+    //             <div className="mint_input">
+    //                 <button disabled={counter <= 0 ? true : false} onClick={decrementCounter}>-</button>
+    //                 <input type="text" name="" id="" value={counter} maxLength={2} />
+    //                 <button disabled={counter >= 10 ? true : false} onClick={incrementCounter}>+</button>
+    //             </div>
+    //             <div className="mint_button">
+    //                 {
+    //                     isPaused ? (
+    //                         <button disabled >Mint is Paused</button>
+    //                     ) : (
+    //                         isPresale && !isPaused ? (
+    //                             <button onClick={() => mintPreSale(web3provider, counter)}>Mint Presale</button>
+    //                         ) : (
+    //                             <button onClick={() => mintPublicSale(web3provider, counter)}>Mint</button>
+    //                         )
+    //                     )
+    //                 }
+    //                 {/* <button onClick={mint}>Minit</button> */}
+    //             </div>
+    //         </div>
+    //     </Section>
     )
 }
